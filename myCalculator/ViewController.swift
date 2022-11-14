f0509a5b8912ac48663c4bcc6666b6d27cdc3abc
Lucas Suarez <lucas.suarez.dev@gmail.com>
2022-11-14 00:10:37 -0400

    Initial Commit

---
+++ myCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myCalculator
+//
+//  Created by Lucas Suarez Blanco on 11/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
